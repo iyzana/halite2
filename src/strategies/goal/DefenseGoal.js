9c5e59f06931a24f67e11631352f610b670f7f74
Henri Schmidt <henri@reschmi.de>
2017-11-18 21:06:12 +0100

    fix some errors in code

--- src/strategies/goal/DefenseGoal.js
+++ src/strategies/goal/DefenseGoal.js
@@ -15,29 +15,30 @@ class DefenseGoal {
 
         const distances = gameMap.enemyShips
             .filter(ship => {
-               const previousShip = gameMap.previous.shipById(ship.id);
-               if (!previousShip) return false;
+                const previousShip = gameMap.previous.shipById(ship.id);
+                if (!previousShip) return false;
 
-               const length = 100;
+                const length = 100;
 
-               const end = {
-                   x: ship.x + (ship.x - previousShip.x) * length,
-                   y: ship.y + (ship.y - previousShip.y) * length,
-               };
+                const end = {
+                    x: ship.x + (ship.x - previousShip.x) * length,
+                    y: ship.y + (ship.y - previousShip.y) * length,
+                };
 
-               //ship is flying in the direction of our planet
-               return Geometry.intersectSegmentCircle(ship, end, this.planet, Constants.DOCK_RADIUS);
+                //ship is flying in the direction of our planet
+                return Geometry.intersectSegmentCircle(ship, end, this.planet, Constants.DOCK_RADIUS);
             })
-            .filter(ship => gameMap.obstaclesBetween(ship, this.planet) > 0)
+            .filter(ship => gameMap.obstaclesBetween(ship, this.planet).length === 0)
             .map(ship =>
-                    this.planet.dockedShips
-                        .map(docked => [docked, Geometry.distance(docked, ship)]
-                        .sort((a, b) => b[1] - a[1]))
-                )
-            .sort((a, b) => b[1] - a[1]);
+                this.planet.dockedShips
+                    .map(docked => [docked, Geometry.distance(docked, ship)]
+                        .sort((a, b) => a[1] - b[1]))
+                    [0]
+            )
+            .sort((a, b) => a[1] - b[1]);
 
         //No defense needed
-        if(distances.length === 0) {
+        if (distances.length === 0) {
             return [];
         }
 
@@ -45,27 +46,33 @@ class DefenseGoal {
         const distance = distances[0][1];
         const turnsTillArrival = distance / Constants.MAX_SPEED;
 
+        if (Simulation.turnsTillNextShip(this.planet) < turnsTillArrival) {
+            return [];
+        }
+
         const sortedShipsInRange = gameMap.myShips
             .filter(ship => ship.isUndocked())
             .map(ship => [ship, Geometry.distance(ship, this.endangeredShip)])
             .filter(tuple => tuple[1] < distance)
-            .sort((a, b) => a[1] - b[1]);
+            .sort((a, b) => b[1] - a[1]);
 
-        if (sortedShipsInRange.length === 0 &&
-            !this.planet.dockedShips.some(ship => ship.isUndocking()) &&
-            Simulation.turnsTillNextShip(this.planet) < turnsTillArrival) {
+        if (sortedShipsInRange.length === 0) {
+            if (!this.planet.dockedShips.some(ship => ship.isUndocking())) {
+                const intents = [new GoalIntent(this.endangeredShip, this, 1)];
+                if (turnsTillArrival < Constants.DOCK_TURNS &&
+                    this.planet.dockedShips.length > 1) {
 
-            const intents = [new GoalIntent(this.endangeredShip, this, 1)];
-            if (turnsTillArrival < Constants.DOCK_TURNS) {
-                if (this.planet.dockedShips.length > 1) {
                     const ship = this.planet.dockedShips
                         .filter(ship => ship !== this.endangeredShip)[0];
 
                     intents.push(new GoalIntent(ship, this, 1));
                 }
+                return intents;
             }
-            return intents;
+
+            return [];
         }
+
 
         const maxDistance = sortedShipsInRange[0][1];
 
